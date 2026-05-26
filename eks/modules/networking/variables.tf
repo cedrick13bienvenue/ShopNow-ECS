67e0f1e604f8793fc3428c54002eef9a097f3b57
bienvenue cedrick <cedrick4smith@gmail.com>
2026-05-26 22:39:21 +0200

    feat(eks/networking): add networking module variables and outputs

---
+++ eks/modules/networking/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "Tag prefix applied to all resources"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
